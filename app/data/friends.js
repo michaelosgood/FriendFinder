79cb25274701d84f7c6d6ae1d51c0cd61fbbe3d5
Michael Osgood <michael.f.osgood@gmail.com>
2018-03-03 18:08:08 -0800

    updated friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,54 +1,102 @@
 // Michael Osgood / FriendFinder Application
 
 var friends = [
-  {
-      name: "Alex",
-      photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/180942_10150150668451070_2498866_n.jpg?oh=1f94498108f9d0ee75e33acbea5e4a22&oe=5A5B9560",
-      scores: [
-      "4",
-      "3",
-      "3",
-      "2",
-      "1",
-      "5",
-      "5",
-      "2",
-      "3",
-      "5",
-      ]
+    {
+        name: "Alex",
+        photo: "https://commons.wikimedia.org/wiki/File:AlexPic.png",
+        scores: [
+        "4",
+        "3",
+        "3",
+        "2",
+        "1",
+        "5",
+        "5",
+        "2",
+        "3",
+        "5",
+        ]
+    },
+    {
+        name: "Sara",
+        photo: "https://commons.wikimedia.org/wiki/File:SaraPic.png",
+        scores: [
+        "2",
+        "3",
+        "3",
+        "1",
+        "1",
+        "2",
+        "2",
+        "2",
+        "2",
+        "3",
+        ]
   },
   {
-      name: "Anthony Osgood",
-      photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/10559713_10154426952990066_1579320126321819151_n.jpg?oh=36a3aea9a83ce29ce1c78697a712b092&oe=5A13746A",
-      scores: [
-      "2",
-      "3",
-      "3",
-      "1",
-      "1",
-      "2",
-      "2",
-      "2",
-      "2",
-      "3",
-      ]
+        name: "Eric",
+        photo: "https://commons.wikimedia.org/wiki/File:EricPic.png",
+        scores: [
+        "5",
+        "4",
+        "5",
+        "4",
+        "5",
+        "3",
+        "5",
+        "4",
+        "3",
+        "5",
+        ]
   },
   {
-      name: "Michael Osgood",
-      photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1896961_10154586102945301_6508286979488237397_n.jpg?oh=94e4bb8d704b9ea5d07476a385fde676&oe=5A5657F3",
-      scores: [
-      "5",
-      "4",
-      "5",
-      "4",
-      "5",
-      "3",
-      "5",
-      "4",
-      "3",
-      "5",
-      ]
-  }
+        name: "Kim",
+        photo: "https://commons.wikimedia.org/wiki/File:KimPic.png",
+        scores: [
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "3",
+        "5",
+        "4",
+        "3",
+        "5",
+        ]
+ },
+  {
+        name: "Yvonne",
+        photo: "https://commons.wikimedia.org/wiki/File:YvonnePic.png",
+        scores: [
+        "5",
+        "3",
+        "5",
+        "4",
+        "3",
+        "3",
+        "5",
+        "4",
+        "5",
+        "5",
+        ]
+  },
+  {
+        name: "Karen",
+        photo: "https://commons.wikimedia.org/wiki/File:KarenPic.png",
+        scores: [
+        "1",
+        "2",
+        "3",
+        "2",
+        "1",
+        "1",
+        "2",
+        "2",
+        "3",
+        "1",
+        ]
+}
 ];
 
 module.exports = friends;
